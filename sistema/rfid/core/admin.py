5f2506ebb6d4b3f9dd995362fab56587c01d9054
Embrace <silviolleite@gmail.com>
2018-11-07 08:31:25 -0200

    Model place

--- sistema/rfid/core/admin.py
+++ sistema/rfid/core/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.utils.timezone import now
 
-from rfid.core.models import Tag, Access_log, User
-
+from rfid.core.models import Tag, Access_log, User, Place
 
 admin.site.site_header = 'Sistema de Controle RFID'
 admin.site.site_title = "Painel Administrativo"
@@ -10,25 +8,30 @@ admin.site.index_title = "Bem vindo ao sistema de controle RFID"
 
 
 class UserAdmin(admin.ModelAdmin):
-  list_display = ('name', 'email')
+    list_display = ('name', 'email')
 
 
 class TagAdmin(admin.ModelAdmin):
-  list_display = ('tag', 'user', 'created_at', 'updated_at', 'active')
+    filter_horizontal = ['places']
+    list_display = ('tag', 'user', 'created_at', 'updated_at', 'active', 'locais')
 
-  def active(self, obj):
-    return obj.state == 1
+    def active(self, obj):
+        return obj.state == 1
 
-  active.short_description = 'Status'
-  active.boolean = True
+    def locais(self, obj):
+        return '  |  '.join([other.name for other in obj.places.all()])
+
+    active.short_description = 'Status'
+    active.boolean = True
 
 
 class LogAdmin(admin.ModelAdmin):
-  list_display = ('user', 'tag', 'created_at')
-  date_hierarchy = 'created_at'
-  list_filter = ('user', 'created_at')
+    list_display = ('user', 'tag', 'created_at')
+    date_hierarchy = 'created_at'
+    list_filter = ('user', 'created_at')
 
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Access_log, LogAdmin)
 admin.site.register(User, UserAdmin)
+admin.site.register(Place)
--- sistema/rfid/core/models.py
+++ sistema/rfid/core/models.py
@@ -1,45 +1,57 @@
 from django.db import models
-from django.utils.datetime_safe import datetime
 
 
 class User(models.Model):
-  name = models.CharField('nome', max_length=255)
-  email = models.CharField('email', max_length=255)
-  created_at = models.DateTimeField('criado em', auto_now_add=True)
-  updated_at = models.DateTimeField('atualizado em', auto_now=True)
+    name = models.CharField('nome', max_length=255)
+    email = models.CharField('email', max_length=255)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now=True)
 
-  class Meta:
-    verbose_name = 'usuário'
-    verbose_name_plural = 'usuário'
+    class Meta:
+        verbose_name = 'usuário'
+        verbose_name_plural = 'usuário'
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField('nome', max_length=100)
+
+    class Meta:
+        verbose_name = 'local'
+        verbose_name_plural = 'locais'
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
-  tag = models.CharField('tag', max_length=8)
-  user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='usuário')
-  state = models.SmallIntegerField('status')
-  created_at = models.DateTimeField('criado em', auto_now_add=True)
-  updated_at = models.DateTimeField('atualizado em', auto_now=True)
+    tag = models.CharField('tag', max_length=8)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='usuário')
+    state = models.SmallIntegerField('status')
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now=True)
+    places = models.ManyToManyField('Place', verbose_name='locais', blank=True)
 
-  class Meta:
-    verbose_name = 'tag'
-    verbose_name_plural = 'tags'
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
 
-  def __str__(self):
-    return self.tag
+    def __str__(self):
+        return self.tag
 
 
 class Access_log(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='usuário')
-  tag = models.ForeignKey('Tag', on_delete=models.CASCADE, verbose_name='tag')
-  status = models.SmallIntegerField('status')
-  created_at = models.DateTimeField('criado em', auto_now_add=True, null=True)
-  updated_at = models.DateTimeField('atualizado em', auto_now=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='usuário')
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE, verbose_name='tag')
+    status = models.SmallIntegerField('status')
+    created_at = models.DateTimeField('criado em', auto_now_add=True, null=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now=True, blank=True)
 
-  class Meta:
-    verbose_name = 'log'
-    verbose_name_plural = 'logs'
+    class Meta:
+        verbose_name = 'log'
+        verbose_name_plural = 'logs'
 
-  def __str__(self):
-    return str(self.id)
+    def __str__(self):
+        return str(self.id)
